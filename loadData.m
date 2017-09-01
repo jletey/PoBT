88c33e952fd841069d348b02d106ee1c0af086d1
John Letey <30328854+JohnLetey@users.noreply.github.com>
2017-09-01 07:10:56 -0600

    Updated loadData.m

--- loadData.m
+++ loadData.m
@@ -3,8 +3,9 @@
 % All Rights Reserved
 % Author: John Letey
 
-% Ask the user to enter in the name of the fasta file
-filename = input('Please enter in the name of your fasta file : ', 's');
+% Get the filename from input.txt
+input = textread('input.txt', '%s', 'delimiter', '\n');
+filename = input{1, 1};
 % Get the file extension
 [ ~, ~, ext ] = fileparts(filename);
 % Check to see if the file is a fasta file
@@ -14,11 +15,9 @@ if strcmp(ext, '.fasta') == 1
     % The function fastaread returns a structure, but it's way easier to 
     % work with cell array, so let's convert data into a cell array
     data = struct2cell(data);
-    % Tell the user that the file has been loaded
-    disp('Your fasta file has been loaded succesfully');
 else
     % Display the error to the user
-    disp('You did not enter a .fasta file');
+    disp('You did not enter in the name of a fasta file into input.txt');
 end
 
 % Note that this script doesn't return anything, but the variable data
