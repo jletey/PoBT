40a1822a103c64dbdc850afaa96455c9465b925c
John Letey <30328854+JohnLetey@users.noreply.github.com>
2017-09-03 11:46:24 -0600

    Updated main.m

--- main.m
+++ main.m
@@ -7,9 +7,13 @@
 run('loadData.m');
 % Load the PSSM from the tamo file
 run('loadPSSM.m');
-% Get the name of the chromosome
+% Get the name of the chromosome from input.txt
 input = textread('input.txt', '%s', 'delimiter', '\n');
 chr = input{2, 1};
+% Get the threshold values from input.txt
+input = textread('input.txt', '%s', 'delimiter', '\n');
+weakThresh = input{5, 1};
+strongThresh = input{6, 1};
 % Break up chr into segments the same length as the PSSM
 
 % Evaluate the PSSMs
