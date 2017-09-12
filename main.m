9f38394816625b13077a557a313878ca80ec7ea4
John Letey <30328854+JohnLetey@users.noreply.github.com>
2017-09-12 09:17:58 -0600

    Updated main.m

--- main.m
+++ main.m
@@ -12,8 +12,8 @@ input = textread('input.txt', '%s', 'delimiter', '\n');
 chrName = input{2, 1};
 % Get the threshold values from input.txt
 input = textread('input.txt', '%s', 'delimiter', '\n');
-weakThresh = input{5, 1};
-strongThresh = input{6, 1};
+weakThresh = str2num(input{5, 1});
+strongThresh = str2num(input{6, 1});
 % Get the chromosome
 chrNumber = chrName(4);
 chrNumber = str2num(chrNumber);
@@ -26,22 +26,38 @@ chr = cellstr(reshape(chr, lenOfPSSM, [])');
 fileID = fopen('output.txt', 'w');
 fprintf(fileID, '%s %s \n', '# searching', chrName);
 fprintf(fileID, '%s %s \n', '# for transcription factor', TF);
-fprintf(fileID, '%s %s \n', '# weak threshold:', weakThresh);
-fprintf(fileID, '%s %s \n', '# strong threshold:', strongThresh);
+fprintf(fileID, '%s %s \n', '# weak threshold:', num2str(weakThresh));
+fprintf(fileID, '%s %s \n', '# strong threshold:', num2str(strongThresh));
 fprintf(fileID, '# \n');
 fprintf(fileID, '# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n');
 fprintf(fileID, '# \n');
 fprintf(fileID, '# %10s %10s %10s \n', 'position', 'strength', 'type');
-% Evaluate all the chunks of the chromosome with the PSSM
+% Find the maximum possible probability
+max = 1;
+for i = 1:lenOfPSSM
+    maxForCollumn = 0;
+    for j = 1:4
+        if PSSM{j, i} > maxForCollumn
+            maxForCollumn = PSSM{j, i};
+        end
+    end
+    max = max*maxForCollumn;
+end
+% Evaluate all the chunks of the chromosome with the PSSM and count how
+% many strong and weak sites there are
 out = {};
+weakAmount = 0;
+strongAmount = 0;
 for i = 1:amount
-    output = outputOfPSSM(PSSM, chr{i, 1});
+    output = outputOfPSSM(PSSM, chr{i, 1}, max);
     out = horzcat(out, output);
     if (output >= weakThresh)
         if (output < strongThresh)
-            fprintf(fileID, '  %10d   %10d %6s \n', i, output, 'weak');
+            fprintf(fileID, '  %10d   %10s %6s \n', i, num2str(output), 'weak');
+            weakAmount = weakAmount + 1;
         else
-            fprintf(fileID, '  %10d   %10d %8s \n', i, output, 'strong');
+            fprintf(fileID, '  %10d   %10s %8s \n', i, num2str(output), 'strong');
+            strongAmount = strongAmount + 1;
         end
     end
 end
