8ef7352f80ad16245a566ae275ac2da01e257358
John Letey <john.letey@colorado.edu>
2017-12-06 16:14:35 -0700

    Updated run.sh, added some features

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Running my Python code \n"
-python Python\ Code/main.py y 2 n n
+echo "Running Python code"
+echo "----------------------"
+printf "\n"
+python Python\ Code/main.py y 2 n n Python\ Code/Hit\ Files
