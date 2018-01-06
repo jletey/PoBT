b33a3a691734599e301066ad8883b7ad4c58eff9
John Letey <john.letey@colorado.edu>
2018-01-06 08:07:07 -0700

    don't need matlab code anymore

--- MATLAB Code/main.m
+++
@@ -1,9 +0,0 @@
-% main implementation
-% Copyright (2017) University of Colorado
-% All Rights Reserved
-% Author: John Letey
-
-% Call hits.m
-run('hits.m');
-% Call distances.m
-run('distances.m');
