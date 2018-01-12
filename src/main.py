08bced59b3141119bc05a679eba583fb3b936191
John Letey <30328854+JohnLetey@users.noreply.github.com>
2018-01-12 10:53:47 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 ## Imports
+from get_chrs import getCHR
 import Bio
 from Bio import SeqIO
 import pandas as pd
@@ -29,23 +30,6 @@ if choice == 'n':
     	path = path[:len(path)-1]
 else:
     path = 'Data'
-## Implementation of getCHRs
-def getCHRs(filename, numUseless):
-    # Tell the user the program is getting the fasta data
-    print('Getting the fasta data out of the file:', filename)
-    # Get the data from the fasta file
-    with open(filename) as fasta_file:
-        identifiers = []
-        seq = []
-        for seq_record in SeqIO.parse(fasta_file, 'fasta'):
-            identifiers.append(seq_record.id)
-            seq.append(str(seq_record.seq))
-    # Put the data into a dataframe
-    s1 = pd.Series(identifiers[:len(identifiers)-numUseless], name='chromosome')
-    s2 = pd.Series(seq[:len(seq)-numUseless], name='sequence')
-    fastaDF = pd.DataFrame(dict(chromosome=s1, sequence=s2))
-    # Return the dataframe
-    return fastaDF
 ## Get the fasta data
 # Ask the user how much data they want to throw away
 if len(args) > 0:
