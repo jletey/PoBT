c662925764ac86b986d11d0d5ac0cb52f30c0106
John Letey <john.letey@colorado.edu>
2018-01-27 11:35:49 -0700

    updated main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from pyfaidx import Fasta
 ## Get the parameters when the user called to function
 args = list(sys.argv[1:])
 ## Tell the user that all data will be grabbed from the data section
-print('All data used will be grabbed from the data section (titled Data)')
+print('All data used will be grabbed from the data section (titled data)')
 if len(args) > 0:
     choice = args[0]
     print('Are you okay with this? [y/n]', choice)
@@ -22,7 +22,7 @@ if choice == 'n':
     if path[len(path)-1] == '/':
     	path = path[:len(path)-1]
 else:
-    path = 'Data'
+    path = 'data'
 ## Get the fasta data
 # Ask the user how much data they want to throw away
 if len(args) > 0:
@@ -37,3 +37,18 @@ else:
 genes = Fasta(path + '/SGDv3.fasta')
 print(type(genes))
 print(genes[0])
+# Get the amount of chromosomes
+# countCHR = fastaDF.count()
+# numOfCHR = countCHR['chromosome']
+# Show the fasta data
+if len(args) > 0:
+    if isDifferentDataPath:
+        choice = args[3]
+    else:
+        choice = args[2]
+    print('Show the fasta data? [y/n]', choice)
+else:
+    choice = str(input('Show the fasta data? [y/n] '))
+if choice == 'y':
+    for key in genes.keys():
+        print(genes[key])
