5d4c38719a24127d53fde9ab6335f1fc516fb25d
John Letey <john.letey@colorado.edu>
2018-02-04 11:33:01 -0700

    started implementing tests

--- ci/travis/test.py
+++ ci/travis/test.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 ## Imports
 import unittest
+## Implementation of test_get_pssm
+def test_get_pssm():
+    # Import get_pssm
+    from get_pssm import get_pssm
 ## Test my code
-test_get_pssm = unittest.TestSuite()
-runner = unittest.TextTestRunner(verbosity=2).run(test_get_pssm)
+test_code = unittest.TestSuite()
+test_code.addTest(test_get_pssm)
+runner = unittest.TextTestRunner(verbosity=2).run(test_code)
