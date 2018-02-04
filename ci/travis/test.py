c019244dfdd9597b49c2191685b39d5fd8013256
John Letey <30328854+johnletey@users.noreply.github.com>
2018-02-04 10:21:12 -0700

    renamed the test program

--- ci/travis/test.py
+++ ci/travis/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ## Imports
 import unittest
-## Test get_pssm
+## Test my code
 test_get_pssm = unittest.TestSuite()
 runner = unittest.TextTestRunner(verbosity=2).run(test_get_pssm)
