85558c4824057435cbd78eada7c812dc8f6e4478
John Letey <john@econ1-172-208-dhcp.int.colorado.edu>
2018-03-21 13:47:32 -0600

    updated the plot histogram function

--- src/plot_hist.py
+++ src/plot_hist.py
@@ -10,60 +10,60 @@
 ## Imports
 from time import time
 from read_gff import read_gff
+import matplotlib.pylab as plt
+plt.style.use('bmh')
+import numpy as np
 ## Implementation of plot_hist
 def plot_hist(HitsInFilename, PicFile):
     # Get the time at the begining of the function
     t_beg = time()
     # Get the data from the GFF file
     data = read_gff(HitsInFilename)
-    print(data[0])
-#     # Sort the lists holding the hits
-#     weakSites = list(set(weakSites))
-#     weakSites.sort()
-#     strongSites = list(set(strongSites))
-#     strongSites.sort()
-#     # Close the input file that holds the hits
-#     fileID.close()
-#     # Read in the input file
-#     Input = readInInput(InFilename)
-#     # Get the number of plus or minus range of what we're going to be anaylsing and the size of the buckets
-#     r = Input[5]
-#     s = Input[4]
-#     # Analyse the hits
-#     categories = [0 for i in range(r)]
-#     #for i in range(len(strongSites)):
-#     #    for j in range(len(weakSites)):
-#     #        value = abs(strongSites[i] - weakSites[j])
-#     #        for k in range(r//s):
-#     #            if (value <= (s*k) and (value > s*(k-1))):
-#     #                categories[k] += 1
-#     for i in range(len(strongSites)):
-#         position1 = 0
-#         position2 = 0
-#         for j in range(len(weakSites)):
-#             if weakSites[j] <= strongSites[i]-r:
-#                 position1 = j
-#             if weakSites[j] <= strongSites[i]+r:
-#                 position2 = j
-#         for j in range(position1, position2):
-#             value = abs(strongSites[i] - weakSites[j])
-#             for k in range(r//s):
-#                 if (value <= (s*k) and (value > s*(k-1))):
-#                     categories[k] += 1
-#     # Plot the histogram
-#     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,5))
-#     ax.bar(range(len(categories)), categories, color='black', label='Histogram')
-#     plt.plot(range(len(categories)), [mean(categories) for i in range(len(categories))], color='red', label='Mean')
-#     plt.plot(range(len(categories)), [2*std(categories)+mean(categories) for i in range(len(categories))], color='steelblue', label='Standard Deviation (+2)')
-#     plt.plot(range(len(categories)), [std(categories)+mean(categories) for i in range(len(categories))], color='cyan', label='Standard Deviation (+1)')
-#     if mean(categories)-std(categories) >= 0:
-#         plt.plot(range(len(categories)), [mean(categories)-std(categories) for i in range(len(categories))], color='cyan', label='Standard Deviation (-1)')
-#     if mean(categories)-2*std(categories) >= 0:
-#         plt.plot(range(len(categories)), [mean(categories)-2*std(categories) for i in range(len(categories))], color='steelblue', label='Standard Deviation (-2)')
-#     ax.legend()
-#     fig.savefig(PicFile)
-#     plt.show()
-#     # Get the time at the end of the function
-#     t_end = time()
-#     # Return the amount of seconds it took the function to run (in seconds)
-#     return t_end - t_beg
+    # Sort the data into sets of weak sites and strong sites
+    weakSites, strongSites = [], []
+    for i in range(len(data)):
+        if data[i][len(data[i])-1] == 'weak':
+            weakSites.append(int(data[i][3]))
+        else:
+            strongSites.append(int(data[i][3]))
+    # Set the number of plus or minus range of what we're going to be anaylsing and the size of the buckets
+    r = 50
+    s = 150
+    # Analyse the hits
+    categories = [0 for i in range(r)]
+    for i in range(len(strongSites[:100])):
+        for j in range(len(weakSites[:100])):
+            value = abs(strongSites[i] - weakSites[j])
+            for k in range(r//s):
+                if (value <= (s*k) and (value > s*(k-1))):
+                    categories[k] += 1
+    # for i in range(len(strongSites)):
+    #     position1 = 0
+    #     position2 = 0
+    #     for j in range(len(weakSites)):
+    #         if weakSites[j] <= strongSites[i]-r:
+    #             position1 = j
+    #         if weakSites[j] <= strongSites[i]+r:
+    #             position2 = j
+    #     for j in range(position1, position2):
+    #         value = abs(strongSites[i] - weakSites[j])
+    #         for k in range(r//s):
+    #             if (value <= (s*k) and (value > s*(k-1))):
+    #                 categories[k] += 1
+    # Plot the histogram
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,5))
+    ax.bar(range(len(categories)), categories, color='black', label='histogram')
+    plt.plot(range(len(categories)), [np.mean(categories) for i in range(len(categories))], color='#a76c6e', label='mean')
+    plt.plot(range(len(categories)), [2*np.std(categories)+np.mean(categories) for i in range(len(categories))], color='steelblue', label='Standard Deviation (+2)')
+    plt.plot(range(len(categories)), [np.std(categories)+np.mean(categories) for i in range(len(categories))], color='gray', label='Standard Deviation (+1)')
+    if np.mean(categories)-np.std(categories) >= 0:
+        plt.plot(range(len(categories)), [np.mean(categories)-np.std(categories) for i in range(len(categories))], color='gray', label='Standard Deviation (-1)')
+    if np.mean(categories)-2*np.std(categories) >= 0:
+        plt.plot(range(len(categories)), [np.mean(categories)-2*np.std(categories) for i in range(len(categories))], color='steelblue', label='Standard Deviation (-2)')
+    ax.grid(alpha=0.25)
+    ax.legend(loc="upper left", fontsize=12)
+    fig.savefig(PicFile)
+    # Get the time at the end of the function
+    t_end = time()
+    # Return the amount of seconds it took the function to run (in seconds)
+    return t_end - t_beg
